0cfd7c75480c2464acd740eb3013a8d4ee1086fb
ask josephsen <askbar@gmail.com>
2017-11-12 22:52:05 +0100

    fetching data - still

---
+++ src/shared/songs-api.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SongsApi {
+
+    // get https://sing-along-10e39.firebaseio.com/songs.json
+    
+    constructor() {
+
+    }
+}
